7d4dc0f8985fb0285885b065e8022dc0c1f980b2
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-07-30 23:38:13 +0900

    Update and rename programmers/Hash/완주하지_못한_사람.py to Hash/완주하지_못한_사람.py

--- Hash/완주하지_못한_사람.py
+++ Hash/완주하지_못한_사람.py
@@ -1,3 +1,5 @@
+Programmers: https://school.programmers.co.kr/learn/courses/30/lessons/42576
+
 # participant	completion	return
 # ["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]	"vinko"
 # ["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
