b090c12a109fa61a81cd90a58d53f7b7cea66cfb
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-08-01 22:11:00 +0900

    Update and rename programmers/heap/더_맵게.py to Heap/더-맵게.py

--- Heap/더-맵게.py
+++ Heap/더-맵게.py
@@ -1,3 +1,5 @@
+Programmers: https://school.programmers.co.kr/learn/courses/30/lessons/42626#
+
 #        scoville	      K	return
 # [1, 2, 3, 9, 10, 12]	7	2
 # 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
@@ -11,16 +13,28 @@
 # scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
 # 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
 
-from heapq import heapify, heappop, heappush
+# collections의 PriorityQueue는 리스트를 한번에 초기화 할 수 없음!! 
+# heapq를 써야한다. 
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while True:
+        if scoville[0] >= K:  return answer
+        elif len(scoville) == 1:  return -1
+        else:
+            heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville)*2)
+            answer += 1
 
+# 예외처리를 하다니 ㅇㅁㅇ
+from heapq import heapify, heappush, heappop
 def solution(scoville, K):
     heapify(scoville)
-    
-    for i in range(len(scoville)-1):
-        tmp = heappop(scoville) + 2*heappop(scoville)
-        heappush(scoville, tmp)
-        
-        if scoville[0] >= K:
-            return i+1
-    #for문 다 돌아도 함수가 안 끝나면...
-    return -1
+    for i in range(1000000):
+        try:
+            heappush(scoville, heappop(scoville)+(heappop(scoville)*2))
+            if scoville[0] >= K: return i+1
+        except:
+            return -1
+
+# queue를 기존값, mix값 2개를 사용해 푼 풀이도 존재함.
