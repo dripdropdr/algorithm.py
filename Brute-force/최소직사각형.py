6591371aedf4ef63c856b6d0af7a2f1c7d9df9ff
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-08-04 00:22:36 +0900

    Update and rename programmers/brute-force/최소직사각형.py to Brute-force/최소직사각형.py

--- Brute-force/최소직사각형.py
+++ Brute-force/최소직사각형.py
@@ -1,3 +1,5 @@
+Programmers: https://school.programmers.co.kr/learn/courses/30/lessons/86491#qna
+
 #               sizes	                            result
 # [[60, 50], [30, 70], [60, 30], [80, 40]]	      4000
 # [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]	  120
@@ -5,19 +7,15 @@
 # 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 
 # 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
 
+
+# 대각선은 항상 자리를 많이 차지함 => 그려보고 판단
+# 한 쪽으로 긴 면 몰아넣기
 def solution(sizes):
-    maxw = 0
-    maxh = 0
-    for size in sizes:
-        if size[0] > size[1]:
-            w = size[0]
-            h = size[1]
-        else:
-            w = size[1]
-            h = size[0]
-        if maxw < w:
-            maxw = w
-        if maxh < h:
-            maxh = h
-    answer = maxw * maxh
-    return answer
+    answer = 0
+    long = []
+    short = []
+    for w, h in sizes:
+        if w>h: long.append(w); short.append(h)
+        else: long.append(h); short.append(w)
+    
+    return max(long) * max(short)
